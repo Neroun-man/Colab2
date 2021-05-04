c2c0dc8e2eff58c2a9a077c2b4f9179c64a1bf6b
Neroun-man <77637953+Neroun-man@users.noreply.github.com>
2021-05-04 21:07:23 +0300

    Update Copy all cells.py

--- Copy all cells.py
+++ Copy all cells.py
@@ -11,7 +11,27 @@ for i in range(1,190):
 
 def f():
   pass
-#maybe somethin new
 
-for i in [1,2]:
-  print(i)
+**Текст, выделенный полужирным шрифтом**
+
+
+
+
+
+
+
+
+
+from sklearn.linear_model import LogisticRegression
+
+logreg = LogisticRegression()
+
+from sklearn.datasets import make_blobs
+
+# Новый раздел
+
+for i in range(1,190):
+  pass
+
+def f():
+  pass
