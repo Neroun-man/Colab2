7cbbd5bb470b583bc83298b20a010a47b7777aaf
Neroun-man <77637953+Neroun-man@users.noreply.github.com>
2021-05-04 21:02:36 +0300

    Create Copy all cells.py

---
+++ Copy all cells.py
@@ -0,0 +1,13 @@
+from sklearn.linear_model import LogisticRegression
+
+logreg = LogisticRegression()
+
+from sklearn.datasets import make_blobs
+
+# Новый раздел
+
+for i in range(1,190):
+  pass
+
+def f():
+  pass
